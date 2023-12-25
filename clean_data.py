0124eabea0f8636a00e53d04a274734357d3165d
BogdanRemusPintilie <127158063+BogdanRemusPintilie@users.noreply.github.com>
2023-12-25 21:23:05 +0000

    Part of cleaning

---
+++ clean_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = r"C:\Users\Bogdan\Desktop\CLEAN\WTS\Convertible bond arbitrage\data.csv"
+
+df = pd.read_csv(data, na_values=["#N/A Field Not Applicable"])
+
+df.dropna(inplace=True)
+
+cleaned_file_path= r"C:\Users\Bogdan\Desktop\CLEAN\WTS\Convertible bond arbitrage\cleaned_data.csv"
+
+df.to_csv(cleaned_file_path, index=False)
