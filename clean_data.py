e0a02f5dfc9ba55ec009112113cca980bd56b910
BogdanRemusPintilie <127158063+BogdanRemusPintilie@users.noreply.github.com>
2023-12-26 01:05:58 +0000

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,8 @@ df = pd.read_csv(data, na_values=["#N/A Field Not Applicable"])
 
 df.dropna(inplace=True)
 
+df = df[df['CV Stock Volatility'] != 0]
+
 cleaned_file_path= r"C:\Users\Bogdan\Desktop\CLEAN\WTS\Convertible bond arbitrage\cleaned_data.csv"
 
 df.to_csv(cleaned_file_path, index=False)
